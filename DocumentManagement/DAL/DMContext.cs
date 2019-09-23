699904287619cff1f2866d6a322eedad1d3484a3
Marek Pisarek <marekpisarek94@gmail.com>
2019-09-23 06:01:04 +0200

    Added document management context db class

---
+++ DocumentManagement/DAL/DMContext.cs
@@ -0,0 +1,22 @@
+﻿using DocumentManagement.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocumentManagement.DAL
+{
+    public class DMContext : DbContext
+    {
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Document> Documents { get; set; }
+
+        public DMContext(DbContextOptions<DMContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
