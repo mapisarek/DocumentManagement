765a8e83df33ffbcdad6cf6e1bc08d118df250c7
Marek Pisarek <marekpisarek94@gmail.com>
2019-09-23 05:54:38 +0200

    Added user model

---
+++ DocumentManagement/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace DocumentManagement.Models
+{
+    public class User
+    {
+        public static ClaimsIdentity Identity { get; set; }
+        [Key]
+        public int UserId { get; set; }
+        [Required]
+        public string UserEmail { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string UserRole { get; set; }
+
+        public virtual ICollection<Document> Documents { get; set; }
+    }
+}
