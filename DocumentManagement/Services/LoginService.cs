d3beb2d6888f1f32100ac4972d5e1a8dee9a9e59
Marek Pisarek <marekpisarek94@gmail.com>
2019-09-23 05:59:00 +0200

    Added login service

---
+++ DocumentManagement/Services/LoginService.cs
@@ -0,0 +1,39 @@
+﻿using DocumentManagement.DAL;
+using DocumentManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocumentManagement.Services
+{
+    public class LoginService
+    {
+        private readonly DMContext _context;
+
+        public LoginService(DMContext dataBase)
+        {
+            _context = dataBase;
+        }
+
+        public List<User> CheckCredential(UserLogin user)
+        {
+            var _user = _context.Users.FirstOrDefault(x => x.UserEmail == user.UserEmail
+             && x.Password == user.Password);
+
+            if (_user == null)
+            {
+                return null;
+            }
+
+            return _context.Users.Where(x => x.UserEmail == _user.UserEmail).Select(x => new User
+            {
+                UserId = x.UserId,
+                UserName = x.UserName,
+                UserEmail = x.UserEmail,
+                UserRole = x.UserRole,
+            }).ToList();
+        }
+    }
+}
+
