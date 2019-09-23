afdc82271c3fe002af4a05ec6a8bdbe36d4012e5
Marek Pisarek <marekpisarek94@gmail.com>
2019-09-23 06:17:21 +0200

    Updated project components and rest of the files

--- DocumentManagement/Program.cs
+++ DocumentManagement/Program.cs
@@ -20,5 +20,6 @@ namespace DocumentManagement
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+                
     }
 }
--- DocumentManagement/wwwroot/js/site.js
+++ DocumentManagement/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+let today = new Date().toISOString().substr(0, 10);
+document.querySelector("#todayDate").value = today;
+
+$(function () {
+    $("#tags input").on({
+        focusout: function () {
+            var txt = this.value.replace(/[^a-z0-9\+\-\.\#]/ig, ''); // allowed characters
+            if (txt) $("<span/>", { text: txt.toLowerCase(), insertBefore: this });
+            this.value = "";
+        },
+        keyup: function (ev) {
+            // if: comma|enter (delimit more keyCodes with | pipe)
+            if (/(188|13)/.test(ev.which)) $(this).focusout();
+        }
+    });
+    $('#tags').on('click', 'span', function () {
+        if (confirm("Remove " + $(this).text() + "?")) $(this).remove();
+    });
+
+});
