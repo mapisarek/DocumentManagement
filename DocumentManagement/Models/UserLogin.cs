48d548b04890b18bd790aaaccc53ad06f0b2c557
Marek Pisarek <marekpisarek94@gmail.com>
2019-09-23 05:53:52 +0200

    Added user login model

---
+++ DocumentManagement/Models/UserLogin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocumentManagement.Models
+{
+    public class UserLogin
+    {
+        [Required]
+        public string UserEmail { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
