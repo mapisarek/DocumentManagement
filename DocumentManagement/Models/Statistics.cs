5d95a09fc3f25a96508ce94bfc89f838b7b5ca6b
Marek Pisarek <marekpisarek94@gmail.com>
2019-09-23 05:53:22 +0200

    Added statistics model

---
+++ DocumentManagement/Models/Statistics.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocumentManagement.Models
+{
+    public class Statistics
+    {
+        public int DocumentsAmount { get; set; }
+        public int CategoriesAmount { get; set; }
+    }
+}
